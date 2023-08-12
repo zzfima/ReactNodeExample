82be82ec866adb4dc78a1420a629926291855a9d
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-08-12 09:45:16 +0300

    add css

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './App.css';
 
 function MainApp() {
   const [backendData, setBackendData] = useState([{}])
@@ -15,19 +16,26 @@ function MainApp() {
     }
   ))
 
-  const mainPage = (
-  <div>
-    {(typeof backendData.adsArea === 'undefined') ? (
-      <p>loading..</p>
-    ) : (
-      backendData.adsArea.map((ad, i) => (
-        <p>{i} : {ad}</p>
-      ))
-    )}
-    <button type="button" onClick={() => setBackendData({ "adsArea": ["Linkedin"] })}>secret ad</button>
-    <button type="button" onClick={() => readUsers()}>Restore</button>
-  </div>)
-
+  const mainPage =
+    <html>
+      <head>
+        <title>Advertisements</title>
+      </head>
+      <body>
+        <h2 className='blueHeader'>Advertisements areas</h2>
+        <div>
+          {(typeof backendData.adsArea === 'undefined') ? (
+            <p>loading..</p>
+          ) : (
+            backendData.adsArea.map((ad, i) => (
+              <p>{i} : {ad}</p>
+            ))
+          )}
+          <button type="button" className='orangeButton' onClick={() => setBackendData({ "adsArea": ["Linkedin"] })}>secret ad</button>
+          <button type="button" onClick={() => readUsers()}>Restore</button>
+        </div>
+      </body>
+    </html>
   return (mainPage)
 }
 
