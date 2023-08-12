cadfb5a41f0b3210e45c203ecf43baede75166c2
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-08-12 08:47:36 +0300

    rename

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -15,19 +15,20 @@ function MainApp() {
     }
   ))
 
-  return (
-    <div>
-      {(typeof backendData.adsArea === 'undefined') ? (
-        <p>loading..</p>
-      ) : (
-        backendData.adsArea.map((ad, i) => (
-          <p>{i} : {ad}</p>
-        ))
-      )}
-      <button type="button" onClick={() => setBackendData({ "adsArea": ["Linkedin"] })}>secret ad</button>
-      <button type="button" onClick={() => readUsers()}>Restore</button>
-    </div>
-  )
+  const mainPage = (
+  <div>
+    {(typeof backendData.adsArea === 'undefined') ? (
+      <p>loading..</p>
+    ) : (
+      backendData.adsArea.map((ad, i) => (
+        <p>{i} : {ad}</p>
+      ))
+    )}
+    <button type="button" onClick={() => setBackendData({ "adsArea": ["Linkedin"] })}>secret ad</button>
+    <button type="button" onClick={() => readUsers()}>Restore</button>
+  </div>)
+
+  return (mainPage)
 }
 
 export default MainApp
